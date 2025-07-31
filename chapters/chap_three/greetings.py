3a63e562b4dce79a0b9dfec52be2f2dd7b827113
Brenleigh Killpack <kil21008@byui.edu>
2025-07-30 19:16:03 -0600

    chapter three start

---
+++ chapters/chap_three/greetings.py
@@ -0,0 +1,4 @@
+names = ['Raylee', 'Madison', 'Callie']
+print(f'{names[0]} is my cool friend')
+print(f'{names[1]} is my cool friend')
+print(f'{names[2]} is my cool friend')
---
+++ chapters/chap_three/transportation.py
@@ -0,0 +1,4 @@
+vehicles = ['car', 'motorcycle', 'bike']
+print(f'I would like to buy a Toyota {vehicles[0]}.')
+print(f'I enjoy riding on my Yamaha {vehicles[1]}.')
+print(f'Someday I will buy a Huffy {vehicles[2]}.')
