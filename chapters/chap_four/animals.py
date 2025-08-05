69ac976dd77561e9722ed8d5dfb3e3609015c458
Brenleigh Killpack <kil21008@byui.edu>
2025-08-04 19:35:56 -0600

    chapter four start

---
+++ chapters/chap_four/animals.py
@@ -0,0 +1,6 @@
+animals = ['Pig', 'Sheep', 'Cow']
+
+for animal in animals:
+  print(f'A {animal} is found on a farm.')
+
+print('You probably have these animals if you live on a farm!')
---
+++ chapters/chap_four/pizza.py
@@ -0,0 +1,6 @@
+pizzas = ['Cheese', 'Pepperoni', 'Hawaiian']
+
+for pizza in pizzas:
+  print(f'I like {pizza} pizza.')
+
+print('I really love pizza!!!')
